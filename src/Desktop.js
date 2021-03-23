6f979c4e479d1ed224b5456729a75242cfdb226f
ggdaltoso <ggdaltoso@gmail.com>
2021-03-22 21:17:47 -0300

    add @react95/clippy

--- src/Desktop.js
+++ src/Desktop.js
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import styled, { createGlobalStyle } from 'styled-components';
 import { GlobalStyle, ThemeProvider, TaskBar, List } from '@react95/core';
+import { ClippyProvider } from '@react95/clippy';
 
 import '@react95/icons/icons.css';
 
@@ -25,37 +26,39 @@ function App() {
 
   return (
     <ThemeProvider>
-      <GlobalStyle />
-      <AppStyle />
+      <ClippyProvider>
+        <GlobalStyle />
+        <AppStyle />
 
-      <Desktop>
-        <ShortcutList>
-          <ShortcutList.Shortcut
-            tabIndex={0}
-            who="eu.jpg"
-            onDoubleClick={() => toggleShowAboutMe(true)}
-          >
-            ggdaltoso
-          </ShortcutList.Shortcut>
-          <ShortcutList.Shortcut tabIndex={1} who="React95-logo.png">
-            React95
-          </ShortcutList.Shortcut>
-        </ShortcutList>
-        {showAboutMe && <AboutMe onClose={() => toggleShowAboutMe(false)} />}
-      </Desktop>
+        <Desktop>
+          <ShortcutList>
+            <ShortcutList.Shortcut
+              tabIndex={0}
+              who="eu.jpg"
+              onDoubleClick={() => toggleShowAboutMe(true)}
+            >
+              ggdaltoso
+            </ShortcutList.Shortcut>
+            <ShortcutList.Shortcut tabIndex={1} who="React95-logo.png">
+              React95
+            </ShortcutList.Shortcut>
+          </ShortcutList>
+          {showAboutMe && <AboutMe onClose={() => toggleShowAboutMe(false)} />}
+        </Desktop>
 
-      <TaskBar
-        list={
-          <List>
-            <List.Item icon="reader_closed_32x32_4bit">
-              Local Disk (C:)
-            </List.Item>
-            <List.Item icon="windows_explorer_32x32_4bit">
-              Windows Explorer
-            </List.Item>
-          </List>
-        }
-      />
+        <TaskBar
+          list={
+            <List>
+              <List.Item icon="reader_closed_32x32_4bit">
+                Local Disk (C:)
+              </List.Item>
+              <List.Item icon="windows_explorer_32x32_4bit">
+                Windows Explorer
+              </List.Item>
+            </List>
+          }
+        />
+      </ClippyProvider>
     </ThemeProvider>
   );
 }
--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -1,5 +1,7 @@
+import { useEffect } from 'react';
 import styled from 'styled-components';
 import { List, Modal, Frame, Fieldset, Icon } from '@react95/core';
+import { useClippy } from '@react95/clippy';
 
 const SocialMediaListItem = styled.li`
   display: flex;
@@ -13,87 +15,131 @@ const SocialMediaListItem = styled.li`
   }
 `;
 
-const AboutMe = ({ onClose }) => (
-  <Modal
-    width="550"
-    icon="ulclient_1002_16x16_4bit"
-    title="Gabriel Gonçalves Daltoso"
-    closeModal={onClose}
-    defaultPosition={{
-      x: 150,
-      y: 250,
-    }}
-    buttons={[{ value: 'Ok', onClick: onClose }]}
-  >
-    <Frame
-      boxShadow="none"
-      bg="transparent"
-      display="flex"
-      flexDirection="column"
-      p={3}
+const AboutMe = ({ onClose }) => {
+  const { clippy } = useClippy();
+
+  useEffect(() => {
+    console.log(clippy);
+    clippy.play('Wave');
+  }, [clippy]);
+
+  return (
+    <Modal
+      width="550"
+      icon="ulclient_1002_16x16_4bit"
+      title="Gabriel Gonçalves Daltoso"
+      closeModal={onClose}
+      buttons={[{ value: 'Ok', onClick: onClose }]}
+      style={{
+        position: 'absolute',
+        left: '20%',
+        top: '20%',
+      }}
     >
-      <Frame boxShadow="none" bg="transparent" display="flex">
-        <Frame
-          width="275px"
-          height="275px"
-          boxShadow="in"
-          flexShrink="0"
-          pt={1}
-          pl={1}
-        >
-          <img alt="foto de Gabriel Daltoso" src="/imgs/eu.jpg" width="100%" />
-        </Frame>
+      <Frame
+        boxShadow="none"
+        bg="transparent"
+        display="flex"
+        flexDirection="column"
+        p={3}
+      >
+        <Frame boxShadow="none" bg="transparent" display="flex">
+          <Frame
+            width="275px"
+            height="275px"
+            boxShadow="in"
+            flexShrink="0"
+            pt={1}
+            pl={1}
+          >
+            <img
+              alt="foto de Gabriel Daltoso"
+              src="/imgs/eu.jpg"
+              width="100%"
+            />
+          </Frame>
 
-        <Frame ml="20" boxShadow="none" bg="transparent" width="100%">
-          <h2 style={{ marginTop: 0 }}>Gabriel Daltoso</h2>
-          <p>
-            Falo que sou front-end mas tento centralizar uma div desde 2014.
-          </p>
-          <p>
-            Atualmente faço isso no
-            <a href="https://github.com/Gympass/yoga">Yoga</a> e quem me paga é
-            a Gympass.
-          </p>
-          <br />
+          <Frame ml="20" boxShadow="none" bg="transparent" width="100%">
+            <h2 style={{ marginTop: 0 }}>Gabriel Daltoso</h2>
+            <p>
+              Falo que sou front-end mas tento centralizar uma div desde 2014.
+            </p>
+            <p>
+              Atualmente faço isso no{' '}
+              <a
+                target="_blank"
+                rel="noreferrer"
+                href="https://github.com/Gympass/yoga"
+              >
+                Yoga
+              </a>{' '}
+              e quem me paga é a Gympass.
+            </p>
+            <br />
 
-          <Fieldset legend="Links úteis" style={{ marginTop: 10 }}>
-            <ul>
-              <SocialMediaListItem>
-                <Icon name="bat_exec_2_32x32_4bit" />
-                <a href="https://github.com/ggdaltoso">github.com/ggdaltoso</a>
-              </SocialMediaListItem>
-              <SocialMediaListItem>
-                <Icon name="info_bubble_32x32_4bit" />
-                <a href="https://twitter.com/ggdaltoso">
-                  twitter.com/ggdaltoso
-                </a>
-              </SocialMediaListItem>
-              <SocialMediaListItem>
-                <Icon name="syncui_120_16x16_4bit" />
-                <a href="https://linkedin.com/in/ggdaltoso">
-                  linkedin.com/in/ggdaltoso
-                </a>
-              </SocialMediaListItem>
-              <SocialMediaListItem>
-                <Icon name="file_pencil_32x32_4bit" />
-                <a href="https://ggdaltoso.dev">ggdaltoso.dev</a>
-              </SocialMediaListItem>
-              <SocialMediaListItem>
-                <Icon name="ulclient_1002_16x16_4bit" />
-                <a href="https://react-goiania.ggdaltoso.dev">
-                  react-goiania.ggdaltoso.dev
-                </a>
-              </SocialMediaListItem>
-            </ul>
-          </Fieldset>
+            <Fieldset legend="Links úteis" style={{ marginTop: 10 }}>
+              <ul>
+                <SocialMediaListItem>
+                  <Icon name="bat_exec_2_32x32_4bit" />
+                  <a
+                    target="_blank"
+                    rel="noreferrer"
+                    href="https://github.com/ggdaltoso"
+                  >
+                    github.com/ggdaltoso
+                  </a>
+                </SocialMediaListItem>
+                <SocialMediaListItem>
+                  <Icon name="info_bubble_32x32_4bit" />
+                  <a
+                    target="_blank"
+                    rel="noreferrer"
+                    href="https://twitter.com/ggdaltoso"
+                  >
+                    twitter.com/ggdaltoso
+                  </a>
+                </SocialMediaListItem>
+                <SocialMediaListItem>
+                  <Icon name="syncui_120_16x16_4bit" />
+                  <a
+                    target="_blank"
+                    rel="noreferrer"
+                    href="https://linkedin.com/in/ggdaltoso"
+                  >
+                    linkedin.com/in/ggdaltoso
+                  </a>
+                </SocialMediaListItem>
+                <SocialMediaListItem>
+                  <Icon name="file_pencil_32x32_4bit" />
+                  <a
+                    target="_blank"
+                    rel="noreferrer"
+                    href="https://ggdaltoso.dev"
+                  >
+                    ggdaltoso.dev
+                  </a>
+                </SocialMediaListItem>
+                <SocialMediaListItem>
+                  <Icon name="ulclient_1002_16x16_4bit" />
+                  <a
+                    target="_blank"
+                    rel="noreferrer"
+                    href="https://react-goiania.ggdaltoso.dev"
+                  >
+                    react-goiania.ggdaltoso.dev
+                  </a>
+                </SocialMediaListItem>
+              </ul>
+            </Fieldset>
+          </Frame>
         </Frame>
+        <List.Divider
+          as="span"
+          style={{ marginLeft: 0, marginTop: 30, width: '100%' }}
+        />
       </Frame>
-      <List.Divider
-        as="span"
-        style={{ marginLeft: 0, marginTop: 30, width: '100%' }}
-      />
-    </Frame>
-  </Modal>
-);
+    </Modal>
+  );
+};
 
 export default AboutMe;
